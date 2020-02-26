d4fa6afb95884b79ea2a63551d1208969b224515
Jonathan <jonathan.rendon@gmail.com>
2020-02-26 19:37:49 -0400

    Updates

---
+++ precache-manifest.e5e3a20f736ae420259d33d06c88c9cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53522e288e01fda21a2f6df332aa0a17",
+    "url": "/rendon/index.html"
+  },
+  {
+    "revision": "8f3d609ad27922601c60",
+    "url": "/rendon/static/css/main.f3c574cc.chunk.css"
+  },
+  {
+    "revision": "8a4c06455ed1fbf5e853",
+    "url": "/rendon/static/js/2.2bfd67aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rendon/static/js/2.2bfd67aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3d609ad27922601c60",
+    "url": "/rendon/static/js/main.232a990c.chunk.js"
+  },
+  {
+    "revision": "5218aecbc4968fb5fb63",
+    "url": "/rendon/static/js/runtime-main.1aebd405.js"
+  },
+  {
+    "revision": "e2bc8e5765fbd83c797dbb21e144873c",
+    "url": "/rendon/static/media/logo_m.e2bc8e57.png"
+  }
+]);
